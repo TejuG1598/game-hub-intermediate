dfc6eb7a33fae7eca09736df2242feabd6723552
TejuG1598 <tejaswisai.15@gmail.com>
2024-04-10 19:04:39 -0400

    Intial commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "9a2f91c9a8ff4271b00d5068bdbc47b0",
   },
 });
