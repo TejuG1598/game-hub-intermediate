59eb19039f8d8e1c9db7cad0a696f190c6c5ce8f
TejuG1598 <tejaswisai.15@gmail.com>
2024-04-14 15:48:24 -0400

    fetching games using useQuery

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient from "../services/api-client";
+import useData, { FetchResponse } from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
@@ -17,18 +19,24 @@ export interface Game {
   rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
+const useGames = (gameQuery: GameQuery) =>{
+  const fetchGames = () => {
+    return apiClient.get<FetchResponse <Game>>('/games',{
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+      }
+    }).then(res => res.data)
+  }
+  const {data, error, isLoading} = useQuery<FetchResponse <Game>, Error>({
+    queryKey: ['games',gameQuery],
+    queryFn: fetchGames
+  })
+
+  return {data, error, isLoading};
+}
+
 
 export default useGames;
